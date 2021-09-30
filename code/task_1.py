5e8ff43b2e3bce4f23c1c359694dc0e7b41c344b
LenkaPt <ptacnikova.lenka@email.cz>
2021-09-30 10:57:58 +0200

    english comments and added func annotations

--- code/task_1.py
+++ code/task_1.py
@@ -1,36 +1,39 @@
-'''První úkol.
+"""First task.
 17.9.2021 - 23.9.2021
-Program načítá a zpracovává sdf soubor.
-'''
+Load and process sdf file.
+"""
 
 import argparse
+from typing import List, Union, Dict, Tuple, TextIO
 
-def skip_two_lines(file):
+
+def skip_two_lines(file: TextIO) -> None:
     for _ in range(2):
         file.readline()
 
 
-def fourth_line(file):
-    '''Vrací počet atomů a počet vazeb molekuly'''
+def fourth_line(file: TextIO) -> (int, int):
+    """Returns number of atoms and number of bonds within a molecule"""
     skip_two_lines(file)
     line = file.readline()
-    return (int(line[:3]), int(line[3:6]))
+    # AAABBB
+    return int(line[:3]), int(line[3:6])
 
 
-def print_info_about_molecule(molecule_name, number_of_atoms, number_of_bonds):
-    '''Funkce slouží na výpis název sloučeniny, počet atomů vyskytujících se 
-    v molekule a počet vazeb.'''
-    print(f'Název sloučeniny: {molecule_name}')
-    print(f'Počet atomů: {number_of_atoms}.\nPočet vazeb: {number_of_bonds}.\n')
+def print_info_about_molecule(molecule_name: str, number_of_atoms: int,
+                              number_of_bonds: int) -> None:
+    """Prints name of molecule, number of its atoms
+     and number of its bonds.
+     """
+    print(f'Molecular name: {molecule_name}')
+    print(f'Number of atoms: {number_of_atoms}.\n'
+          f'Number of bonds: {number_of_bonds}.\n')
 
 
-def coordinates(file, number_of_atoms):
-    '''Zpracuje část souboru se souřadnicemi atomů.
-    Ukládá informace o jednotlivých atomech do slovníku.
-    Informace o všech atomech molekuly uloží do seznamu.
-    
-    Vrací seznam slovníků'''
-
+def coordinates(file: TextIO, number_of_atoms: int) -> List[Dict[str, Union[float, str]]]:
+    """Process coordinates part of file.
+    Info about one atom is stored in dictionary.
+    """
     coordinates_of_atoms = []
     for _ in range(number_of_atoms):
         atom = {}
@@ -39,17 +42,14 @@ def coordinates(file, number_of_atoms):
 
         atom["x"], atom["y"], atom["z"], atom["atom name"] = x, y, z, atom_name
         coordinates_of_atoms.append(atom)
-        
+
     return coordinates_of_atoms
 
 
-def bonds(file, number_of_bonds):
-    '''Zpracuje část souboru s informacemi o vazbách.
-    Uloží informace o každé vazbě do tuplu.
-    Informace o vazbách v rámci molekuly jsou uloženy do seznamu.
-    
-    Vrací seznam tuplů.'''
-    
+def bonds(file: TextIO, number_of_bonds: int) -> List[Tuple[int, int, int]]:
+    """Process bonds part of file.
+    Info about one bond is stored in tuple.
+    """
     informations_about_bonds = []
     for i in range(number_of_bonds):
         line = file.readline()
@@ -60,37 +60,39 @@ def bonds(file, number_of_bonds):
     return informations_about_bonds
 
 
-def molecular_formula(number_of_atoms, molecule_data):
-    '''Vypíše sumární vzorec molekuly'''
+def molecular_formula(number_of_atoms: int,
+                      molecule_data: List[Dict[str, Union[float, str]]]) -> None:
+    """Prints molecular formula."""
     atom_names_list = [molecule_data[i]["atom name"] for i in range(number_of_atoms)]
-    print("Sumární vzorec: ", end="")
+    print("Molecular formula: ", end="")
     for atom in (set(atom_names_list)):
         print(f'{atom}{atom_names_list.count(atom)}', end=" ")
     print()
 
 
-def non_structural_data(file):
+def non_structural_data(file: TextIO) -> None:
     delimiter = "$$$$\n"
     line = file.readline()
     while line != delimiter:
         line = file.readline()
 
 
-def number_of_bonds_outgoing_of_atom(atoms_bonds_dict, number_of_atom):
-    '''Vypíše informaci o počtu vazeb vycházejícího z konkrétního atomu'''
-    print(f'Z atomu č. {number_of_atom} vychází {atoms_bonds_dict[number_of_atom][0]}',
-    f'jednoduchých vazeb, {atoms_bonds_dict[number_of_atom][1]} dvojných a ',
-    f'{atoms_bonds_dict[number_of_atom][2]} trojných vazeb.')
+def bonds_of_atom(atoms_bonds_dict: Dict[int, List[int]],
+                  number_of_atom: int) -> None:
+    """Prints information about bonds outgoing of specific atom."""
+    print(f'Atom number {number_of_atom} forms {atoms_bonds_dict[number_of_atom][0]}',
+          f'single bonds, {atoms_bonds_dict[number_of_atom][1]} double bonds and ',
+          f'{atoms_bonds_dict[number_of_atom][2]} triple bonds.')
 
 
-def processed_informations_about_bonds(data_about_bonds, number_of_atoms):
-    '''Fce dává dohromady informace o počtu vazeb vycházejících z atomů.
-    Vrací slovník, kde klíč je pořadí atomu a hodnota je seznam o třech prvcích.
-    Na první pozici je počet jednoduchých vazeb, na druhé počet dvojných vazeb
-    a na třetí pořet trojných vazeb vycházejících z daného atomu.'''
-
+def processed_informations_about_bonds(data_about_bonds: List[Tuple[int, int, int]],
+                                       number_of_atoms: int) -> Dict[int, List[int]]:
+    """Provides informations about atoms and its bonds.
+    Returns dictionary. Key -> number of atom. Value -> list, where
+    position means type of bond formed by atom [single, double, triple].
+    """
     atoms_bonds_dict = {}
-    for i in range(1, number_of_atoms +1):
+    for i in range(1, number_of_atoms + 1):
         atoms_bonds_dict[i] = [0, 0, 0]
 
     for first_atom, sec_atom, type_of_bond in data_about_bonds:
@@ -99,43 +101,44 @@ def processed_informations_about_bonds(data_about_bonds, number_of_atoms):
         atoms_bonds_dict[sec_atom][type_of_bond - 1] = \
             atoms_bonds_dict.get(sec_atom)[type_of_bond - 1] + 1
 
-    number_of_bonds_outgoing_of_atom(atoms_bonds_dict, 2)
+    bonds_of_atom(atoms_bonds_dict, 2)
     return atoms_bonds_dict
 
 
-def statistics(number_of_records, names_of_all_molecules_in_file, \
-    number_of_atoms_of_each_molecule_in_file):
-    '''Vypíše celkový počet molekul v souboru.
-    Největší molekulu a nejmenší molekulu.'''
-    print(f'Počet záznamů v souboru: {number_of_records}')
-    max_atom_count = max(number_of_atoms_of_each_molecule_in_file)
-    print(f'Největší počet atomů má molekula: '
-        f'{names_of_all_molecules_in_file[number_of_atoms_of_each_molecule_in_file.index(max_atom_count)]}'
-        f', konkrétně: {max_atom_count} atomů')
-    min_atom_count = min(number_of_atoms_of_each_molecule_in_file)
-    print(f'Nejmenší počet atomů má molekula: '
-        f'{names_of_all_molecules_in_file[number_of_atoms_of_each_molecule_in_file.index(min_atom_count)]}'
-        f', konkrétně: {min_atom_count} atomů')
+def statistics(number_of_records: int, names_of_all_molecules: List[str],
+               number_of_atoms_of_each_molecule: List[int]) -> None:
+    """Prints out number of all molecules, the largest molecule and
+    smallest molecule.
+    """
+    print(f'Number of molecules: {number_of_records}')
+    max_atom_count = max(number_of_atoms_of_each_molecule)
+    print(f'Highest number of atoms has molecule: '
+          f'{names_of_all_molecules[number_of_atoms_of_each_molecule.index(max_atom_count)]}'
+          f', exactly: {max_atom_count} atoms.')
+    min_atom_count = min(number_of_atoms_of_each_molecule)
+    print(f'Smallest number of atoms has molecule: '
+          f'{names_of_all_molecules[number_of_atoms_of_each_molecule.index(min_atom_count)]}'
+          f', exactly: {min_atom_count} atoms.')
 
 
-def read_mol_file(path_to_file):
+def read_mol_file(path_to_file: str) -> None:
     with open(path_to_file) as file:
-        names_of_all_molecules_in_file = []
-        number_of_atoms_of_each_molecule_in_file = []
+        names_of_all_molecules = []
+        number_of_atoms_of_each_molecule = []
         number_of_records = 0
-        
+
         while True:
             molecule_name = file.readline().strip()
             if molecule_name == "":
                 break
 
-            names_of_all_molecules_in_file.append(molecule_name)
-            
-            number_of_atoms, number_of_bonds = fourth_line(file)
-            number_of_atoms_of_each_molecule_in_file.append(number_of_atoms)
+            names_of_all_molecules.append(molecule_name)
 
-            print_info_about_molecule(molecule_name, number_of_atoms, \
-                number_of_bonds)
+            number_of_atoms, number_of_bonds = fourth_line(file)
+            number_of_atoms_of_each_molecule.append(number_of_atoms)
+
+            print_info_about_molecule(molecule_name, number_of_atoms,
+                                      number_of_bonds)
 
             coordinates_data = coordinates(file, number_of_atoms)
             data_about_bonds = bonds(file, number_of_bonds)
@@ -146,8 +149,8 @@ def read_mol_file(path_to_file):
 
             number_of_records += 1
             print("--------\n")
-        statistics(number_of_records, names_of_all_molecules_in_file, \
-            number_of_atoms_of_each_molecule_in_file)
+        statistics(number_of_records, names_of_all_molecules,
+                   number_of_atoms_of_each_molecule)
 
 
 parser = argparse.ArgumentParser()
