01ff60b1ef4c66086046a3482a8eb173ee31642c
LenkaPt <ptacnikova.lenka@email.cz>
2021-10-05 09:27:02 +0200

    read_pdb_file added

--- code/task_1.py
+++ code/task_1.py
@@ -5,6 +5,7 @@ Load and process sdf file.
 
 import argparse
 from typing import List, Union, Dict, Tuple, TextIO
+from collections import Counter
 
 
 def skip_two_lines(file: TextIO) -> None:
@@ -12,7 +13,7 @@ def skip_two_lines(file: TextIO) -> None:
         file.readline()
 
 
-def fourth_line(file: TextIO) -> (int, int):
+def atoms_bonds_count(file: TextIO) -> (int, int):
     """Returns number of atoms and number of bonds within a molecule"""
     skip_two_lines(file)
     line = file.readline()
@@ -38,10 +39,10 @@ def coordinates(file: TextIO,
     coordinates_of_atoms = []
     for _ in range(number_of_atoms):
         atom = {}
-        x, y, z, atom_name, *rest = file.readline().split()
+        x, y, z, element_name, *rest = file.readline().split()
         x, y, z = float(x), float(y), float(z)
 
-        atom['x'], atom['y'], atom['z'], atom['atom name'] = x, y, z, atom_name
+        atom['x'], atom['y'], atom['z'], atom['element name'] = x, y, z, element_name
         coordinates_of_atoms.append(atom)
 
     return coordinates_of_atoms
@@ -64,18 +65,19 @@ def bonds(file: TextIO, number_of_bonds: int) -> List[Tuple[int, int, int]]:
 def molecular_formula(number_of_atoms: int,
                       molecule_data: List[Dict[str, Union[float, str]]]) -> None:
     """Prints molecular formula."""
-    atom_names_list = [molecule_data[i]['atom name'] for i in range(number_of_atoms)]
+    atom_names_list = [molecule_data[i]['element name'] for i in range(number_of_atoms)]
     print('Molecular formula: ', end='')
-    for atom in (set(atom_names_list)):
-        print(f'{atom}{atom_names_list.count(atom)}', end=' ')
+    for key, value in Counter(atom_names_list).items():
+        print(f'{key}{value}', end=' ')
     print()
 
 
-def non_structural_data(file: TextIO) -> None:
-    delimiter = '$$$$\n'
+def non_structural_data(file: TextIO, delimiter: str) -> str:
+    """Skip lines until the line with delimiter is reached."""
     line = file.readline()
-    while line != delimiter:
+    while not line.startswith(delimiter):
         line = file.readline()
+    return line
 
 
 def bonds_of_atom(atoms_bonds_dict: Dict[int, List[int]],
@@ -94,48 +96,59 @@ def processed_informations_about_bonds(data_about_bonds: List[Tuple[int, int, in
     """
     atoms_bonds_dict = {}
     for i in range(1, number_of_atoms + 1):
+        # atoms_bonds_dict[i] = [single, double, triple] bond
         atoms_bonds_dict[i] = [0, 0, 0]
 
     for first_atom, sec_atom, type_of_bond in data_about_bonds:
-        atoms_bonds_dict[first_atom][type_of_bond - 1] = \
-            atoms_bonds_dict.get(first_atom)[type_of_bond - 1] + 1
-        atoms_bonds_dict[sec_atom][type_of_bond - 1] = \
-            atoms_bonds_dict.get(sec_atom)[type_of_bond - 1] + 1
+        # type_of_bond - 1 -> list index from 0
+        atoms_bonds_dict[first_atom][type_of_bond - 1] += 1
+        atoms_bonds_dict[sec_atom][type_of_bond - 1] += 1
 
     bonds_of_atom(atoms_bonds_dict, 2)
     return atoms_bonds_dict
 
 
-def statistics(number_of_records: int, names_of_all_molecules: List[str],
+def find_min_max(sequence: List[int]) -> (int, int):
+    """Returns index of max and index of min element"""
+    max_index = 0
+    min_index = 0
+    for i in range(len(sequence)):
+        if sequence[i] > sequence[max_index]:
+            max_index = i
+        elif sequence[i] < sequence[min_index]:
+            min_index = i
+    return max_index, min_index
+
+
+def statistics(names_of_all_molecules: List[str],
                number_of_atoms_of_each_molecule: List[int]) -> None:
     """Prints out number of all molecules, the largest molecule and
     smallest molecule.
     """
-    print(f'Number of molecules: {number_of_records}')
-    max_atom_count = max(number_of_atoms_of_each_molecule)
+    max_index, min_index = find_min_max(number_of_atoms_of_each_molecule)
+
+    print(f'Number of molecules: {len(number_of_atoms_of_each_molecule)}')
     print(f'Highest number of atoms has molecule: '
-          f'{names_of_all_molecules[number_of_atoms_of_each_molecule.index(max_atom_count)]}'
-          f', exactly: {max_atom_count} atoms.')
-    min_atom_count = min(number_of_atoms_of_each_molecule)
+          f'{names_of_all_molecules[max_index]}'
+          f', exactly: {number_of_atoms_of_each_molecule[max_index]} atoms.')
     print(f'Smallest number of atoms has molecule: '
-          f'{names_of_all_molecules[number_of_atoms_of_each_molecule.index(min_atom_count)]}'
-          f', exactly: {min_atom_count} atoms.')
+          f'{names_of_all_molecules[min_index]}'
+          f', exactly: {number_of_atoms_of_each_molecule[min_index]} atoms.')
 
 
-def read_mol_file(path_to_file: str) -> None:
+def read_sdf_file(path_to_file: str) -> None:
     with open(path_to_file) as file:
         names_of_all_molecules = []
         number_of_atoms_of_each_molecule = []
-        number_of_records = 0
 
         while True:
             molecule_name = file.readline().strip()
-            if molecule_name == '':
+            if not molecule_name:
                 break
 
             names_of_all_molecules.append(molecule_name)
 
-            number_of_atoms, number_of_bonds = fourth_line(file)
+            number_of_atoms, number_of_bonds = atoms_bonds_count(file)
             number_of_atoms_of_each_molecule.append(number_of_atoms)
 
             print_info_about_molecule(molecule_name, number_of_atoms,
@@ -146,12 +159,77 @@ def read_mol_file(path_to_file: str) -> None:
             processed_informations_about_bonds(data_about_bonds, number_of_atoms)
 
             molecular_formula(number_of_atoms, coordinates_data)
-            non_structural_data(file)
+            non_structural_data(file, '$$$$\n')
 
-            number_of_records += 1
             print('--------\n')
-        statistics(number_of_records, names_of_all_molecules,
-                   number_of_atoms_of_each_molecule)
+        statistics(names_of_all_molecules, number_of_atoms_of_each_molecule)
+
+
+###############################################
+"""Task 2
+24.9.2021 - 5.10.2021
+Load and process pdb file.
+"""
+
+def atom_coordinates_pdb(line: str) -> Dict[str, Union[float, str]]:
+    """Returns coordinates of atom (pdb format)."""
+    atom = {}
+    x, y, z = float(line[31:38]), float(line[39:46]), float(line[47:54])
+    element = line[77:78].strip()
+    atom['x'], atom['y'], atom['z'], atom['element name'] = x, y, z, element
+    return atom
+
+
+def one_aa_from_pdb(line: str, file: TextIO) \
+        -> Tuple[Dict[str, Union[list, int, str]], str]:
+    """Loads info about one aminoacid, its atoms
+    and coordinates of particular atoms.
+    """
+    residue_name, residue_sequence_num = line[17:20], line[23:26]
+    only_aa = {'residue name': residue_name,
+               'residue number': int(residue_sequence_num),
+               'atoms': [],
+               'coordinates': []}
+    while residue_sequence_num == line[23:26]:
+        atom_name = line[13:16].strip()
+        only_aa['atoms'].append(atom_name)
+        only_aa['coordinates'].append(atom_coordinates_pdb(line))
+        line = file.readline()
+        if line.startswith('TER'):
+            # TER -> termination of chain (ignored in this program)
+            line = file.readline()
+            break
+    return only_aa, line
+
+
+def most_common_aa(aa: List[str]) -> Dict[str, int]:
+    """Prints count of particular aminoacids in protein.
+    Prints the most common aminoacid in protein.
+    Returns count of particular aminoacids.
+    """
+    # Counter.most_common returns list of tuples (of x most commons)
+    aa_counts = Counter(aa)
+    print(f'Protein consists of:')
+    for key, value in aa_counts.items():
+        print(f'{value} {key}')
+    print()
+    most_common = aa_counts.most_common(1)[0]
+    print(f'Most common aminoacid is: {most_common[0]} ({most_common[1]} occurrencies).')
+    return aa_counts
+
+
+def read_pdb_file(path_to_file: str) -> None:
+    with open(path_to_file) as file:
+        line = non_structural_data(file, 'ATOM')
+        all_aa_names = []
+        all_aa_info = []
+        while line.startswith('ATOM'):
+            one_aa, line = one_aa_from_pdb(line, file)
+            all_aa_names.append(one_aa['residue name'])
+            all_aa_info.append(one_aa)
+        # print(all_aa_info)
+        print()
+        aa_counts = most_common_aa(all_aa_names)
 
 
 if __name__ == '__main__':
@@ -159,4 +237,8 @@ if __name__ == '__main__':
     parser.add_argument('file', help='Input file in .sdf format')
     args = parser.parse_args()
 
-    read_mol_file(args.file)
+    input_file = args.file
+    if input_file.endswith('.sdf'):
+        read_sdf_file(input_file)
+    elif input_file.endswith('.pdb'):
+        read_pdb_file(input_file)
