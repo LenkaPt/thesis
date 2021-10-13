edbb811611e42000e8c0d401b92400f55bd00442
LenkaPt <ptacnikova.lenka@email.cz>
2021-10-13 14:11:50 +0200

    get_atom_pdb and get_residue_pdb functions added - program can read 20 residues from pdb file

--- code/oop_files.py
+++ code/oop_files.py
@@ -1,5 +1,5 @@
 import argparse
-from typing import List, Union, Dict, Tuple, TextIO
+from typing import List, Dict, Tuple, TextIO
 from collections import Counter
 
 
@@ -51,6 +51,9 @@ class Molecule:
             print(f'{atom}{count}', end='')
         print()
 
+    def __len__(self):
+        return len(self.atoms)
+
 
 '''atom1 = Atom(1, 2, 3, 'H')
 atom2 = Atom(0.1, 0.2, 0.3, 'H')
@@ -60,6 +63,23 @@ atom4 = Atom(1, 2, 3, 'O')
 molec = Molecule('H2O', [atom1, atom2, atom3, atom4], {(1, 12): 1, (1, 17): 1, (1, 18): 1, (2, 16): 2, (3, 17): 2})
 molec.molecular_formula()
 print(molec.atoms)'''
+
+
+class Chain:
+    def __init__(self, residues, name):
+        self.residues = residues
+        self._name = name
+
+
+class Residue:
+    def __init__(self, name, number, atoms):
+        self._name = name
+        self._number = number
+        self._atoms = atoms
+
+    @property
+    def name(self):
+        return self._name
 
 
 def skip_two_lines(file: TextIO) -> None:
@@ -75,9 +95,8 @@ def atoms_bonds_count(file: TextIO) -> (int, int):
     return int(line[:3]), int(line[3:6])
 
 
-def coordinates(file: TextIO,
-                number_of_atoms: int) -> List[Atom]:
-    """Process coordinates part of sdf file."""
+def get_atoms(file: TextIO, number_of_atoms: int) -> List[Atom]:
+    """Saves data about atoms in sdf file."""
     atoms = []
     for _ in range(number_of_atoms):
         x, y, z, element_name, *rest = file.readline().split()
@@ -91,18 +110,18 @@ def coordinates(file: TextIO,
 
 def bonds(file: TextIO, number_of_bonds: int) -> Dict[Tuple[int, int], int]:
     """Process bonds part of sdf file."""
-    informations_about_bonds = {}
+    bonds_data = {}
     for i in range(number_of_bonds):
         line = file.readline()
         first_atom, second_atom = sorted((int(line[:3]), int(line[3:6])))
         type_of_bond = int(line[6:9])
 
-        informations_about_bonds[(first_atom, second_atom)] = type_of_bond
+        bonds_data[(first_atom, second_atom)] = type_of_bond
 
-    return informations_about_bonds
+    return bonds_data
 
 
-def non_structural_data(file: TextIO, delimiter: str) -> str:
+def skip_non_structural_data(file: TextIO, delimiter: str) -> str:
     """Skip lines until the line with delimiter is reached."""
     line = file.readline()
     while not line.startswith(delimiter):
@@ -115,9 +134,9 @@ def find_min_max(molecules: List[Molecule]) -> (Molecule, Molecule):
     max_molecule = molecules[0]
     min_molecule = molecules[0]
     for molecule in molecules:
-        if len(molecule.atoms) > len(max_molecule.atoms):
+        if len(molecule) > len(max_molecule):
             max_molecule = molecule
-        elif len(molecule.atoms) < len(min_molecule.atoms):
+        elif len(molecule) < len(min_molecule):
             min_molecule = molecule
     return max_molecule, min_molecule
 
@@ -143,11 +162,11 @@ def read_sdf_file(path_to_file: str) -> None:
         molecular_name = file.readline().strip()
         while molecular_name:
             number_of_atoms, number_of_bonds = atoms_bonds_count(file)
-            atoms = coordinates(file, number_of_atoms)
-            moj_bond = bonds(file, number_of_bonds)
-            non_structural_data(file, '$$$$\n')
+            atoms = get_atoms(file, number_of_atoms)
+            bonds_data = bonds(file, number_of_bonds)
+            skip_non_structural_data(file, '$$$$\n')
 
-            molecule = Molecule(molecular_name, atoms, moj_bond)
+            molecule = Molecule(molecular_name, atoms, bonds_data)
 
             '''print(molecule.name)
             molecule.molecular_formula()
@@ -160,10 +179,44 @@ def read_sdf_file(path_to_file: str) -> None:
         statistics(molecules)
 
 
+######################
+def get_atom_pdb(line: str) -> Atom:
+    """Returns one atom from pdb file (as instance of Atom class)"""
+    atom_name = line[13:16]
+    x, y, z = float(line[31:38]), float(line[39:46]), float(line[47:54])
+    atom = Atom(x, y, z, atom_name)
+    return atom
+
+
+def get_residue_pdb(file: TextIO, line: str) -> Tuple[Residue, str, str]:
+    """Returns one residuum from pdb file."""
+    chain_id = line[22]
+    residue_seq_number = line[23:26]
+    residue_name = line[17:20]
+    atoms = []
+    while residue_seq_number == line[23:26]:
+        atom = get_atom_pdb(line)
+        atoms.append(atom)
+        line = file.readline()
+    residue = Residue(residue_name, residue_seq_number, atoms)
+
+    return residue, chain_id, line
+
+
+def read_pdb_file(path_to_file: str) -> None:
+    with open(path_to_file) as file:
+        line = skip_non_structural_data(file, 'ATOM')
+        residues = []
+        for i in range(20):
+            residue, chain_id, line = get_residue_pdb(file, line)
+            residues.append(residue)
+            print(residues[i].name)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('file', help='Input file in .sdf or .pdb format')
     args = parser.parse_args()
 
     input_file = args.file
-    read_sdf_file(input_file)
+    read_pdb_file(input_file)
